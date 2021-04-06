1112767ea3d2660d94f7cfae620eee6c2080adf0
CrazyQuackers <omerkohavi2004@gmail.com>
2021-04-06 23:12:58 +0300

    might work

--- Uni/commands/top.js
+++ Uni/commands/top.js
@@ -2,7 +2,7 @@ exports.run = async (bot,message,args) => {
     const guild = bot.guilds.cache.get('823252764246147152')
     const membersArray = guild.members.cache.array()
     membersArray.forEach(member => {
-        console.log(member.username)
+        console.log(member.user.username)
     })
 }
 exports.help = {
