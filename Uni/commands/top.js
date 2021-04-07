4e78d9fa677e05a7b551b2efab215e9b6a441d50
CrazyQuackers <omerkohavi2004@gmail.com>
2021-04-07 09:49:54 +0300

    undefined bug fixed

--- Uni/commands/top.js
+++ Uni/commands/top.js
@@ -7,12 +7,15 @@ exports.run = async (bot,message,args) => {
     let i = 0
     let str = ""
     var topTen = []
+    var removeUndefined = []
     membersArray.forEach(member => {if((!member.user.bot)&&(ez.users[member.user.id])){notBots[i] = member.user}
         i++})
     console.log(notBots)
     var sorted = notBots.sort((a, b) => ez.users[b.id].antenna - ez.users[a.id].antenna)
     console.log(sorted)
-    for(i=0 ; i<sorted.length ; i++){if(i<=9){topTen[i]=sorted[i]}}
+    for(i=0 ; i<sorted.length ; i++){if(i<=9){removeUndefined[i]=sorted[i]}}
+    console.log(removeUndefined)
+    for(i=0 ; i<removeUndefined.length ; i++){if(removeUndefined[i]!=undefined){topTen[i]=removeUndefined[i]}}
     console.log(topTen)
     for(i=0 ; i<topTen.length ; i++){str=str+`${i+1}. **${topTen[i].username}** - ${ez.users[topTen[i].id].antenna} 📡\n`}
     const topEmbed = {
